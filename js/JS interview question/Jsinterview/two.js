eb2535cff79975a07f3ee8beab4f61e5a50f3dd1
pratik <raut70856@gmail.com>
2025-11-08 13:56:15 +0530

    Add two.js file

---
+++ js/JS interview question/Jsinterview/two.js
@@ -0,0 +1,70 @@
+// console.log(1 + 1 -'1')
+
+// let x ={} , y ={name: "Satish"} ,z= {name:"Pratik"}
+
+// x[y] = {
+//     name:"salman"
+// }
+// x[z] = {
+//     name:"Shahrukg"
+// }
+
+// console.log(x[y]) // it will overite the x
+
+
+
+// console.log(NaN === NaN)
+
+// console.log(1>2<3)
+
+// what are higher order function 
+// what is destructuring 
+
+
+// [x,y,...restof] = [10,20,30,40,50]
+// console.log(restof)
+
+// what is turnery operator alternative for turnery operator
+
+// for(var  i = 0; i<3; i++){
+//     setTimeout(() => console.log(i) ,2000)
+// }
+// what concept are we using
+//destructuring of array 
+// let a = 12;
+// let b = 4;
+// [b,a] = [a,b];
+// console.log(a,b)
+
+//reverse a string
+
+let str = "pratik"
+// how can we merge two arrays == spread operator
+//  what is split , reverse and join
+
+//what is  babble 
+// difference in undeclared and undefined  write an example 
+// what is hoisting move at the top in which process == compilation 
+
+
+// console.log(b)
+// let b;
+// difference in let and const 
+
+// what is promise what is pending fullfilled and rejected 
+
+//difference in event capturing and event bubbling
+// lexical scope in js 
+
+//difference in first order and higher order function
+// difference in map filter reduce
+
+// temeplate literals
+
+// array methods in js and splice and slice 
+
+// difference between settimeout and setinterval 
+// if there are two code one is of promise one is of settimeout then which code will run first
+//what is event loop
+// shallow copy and deep copy 
+   
